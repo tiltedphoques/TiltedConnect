fbf869c92883ae17bf074c555126068f73431b99
Tony <toekneegaming123@gmail.com>
2024-09-05 21:07:41 -0400

    tweak: more version pinning

--- xmake.lua
+++ xmake.lua
@@ -6,8 +6,8 @@ set_xmakever("2.5.1")
 add_requires("tiltedcore v0.2.7", "hopscotch-map v2.3.1", "snappy 1.1.10", "gamenetworkingsockets v1.4.1", "catch2 2.13.9", "libuv v1.48.0")
 
 -- dependencies' dependencies version pinning
-add_requireconfs("tiltedcore.mimalloc", { version = "2.1.7", override = true })
-add_requireconfs("gamenetworkingsockets.openssl", { version = "1.1.1-w", override = true })
+add_requireconfs("*.mimalloc", { version = "2.1.7", override = true })
+add_requireconfs("*.openssl", { version = "1.1.1-w", override = true })
 add_requireconfs("*.cmake", { version = "3.30.2", override = true })
 add_requireconfs("*.protobuf*", { version = "26.1", override = true, build = true }) -- needs build or else linker screams
 
