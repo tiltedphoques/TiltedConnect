81f5f352125f405fcbb1d7e7844d42b6f109d14b
Richard Fortier <rwf@ourchateau.net>
2025-05-18 14:37:33 -0400

    Fix xmake.lua for Abseil update
    Abseil update 20250512 is not compatible with the released version of GameNetworkingSockets or protobuf. Protobuf has been updated but the release version of GameNetworkingSockets has not yet been updated.
    
    Abseil wasn't pinned, it is now pinned at 20250127.1 for everything that pulls it in.

--- xmake.lua
+++ xmake.lua
@@ -9,7 +9,8 @@ add_requires("tiltedcore v0.2.7", "hopscotch-map v2.3.1", "snappy 1.1.10", "game
 add_requireconfs("*.mimalloc", { version = "2.1.7", override = true })
 add_requireconfs("*.openssl", { version = "1.1.1-w", override = true })
 add_requireconfs("*.cmake", { version = "3.30.2", override = true })
-add_requireconfs("*.protobuf*", { version = "26.1", override = true, build = true }) -- needs build or else linker screams
+add_requireconfs("*.protobuf*", { version = "26.1", override = true }) 
+add_requireconfs("**.abseil*", { version = "20250127.1", override = true }) 
 
 add_rules("mode.debug","mode.releasedbg", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
